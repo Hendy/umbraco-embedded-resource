0d6ebbd2c38f077fc2832c980c3abf9acd6d2f60
Hendy Racher <git@hendyracher.co.uk>
2018-06-07 09:36:30 +0100

    moved test example resources into a folder

--- src/Our.Umbraco.EmbeddedResource.Tests/Constants.cs
+++ src/Our.Umbraco.EmbeddedResource.Tests/Constants.cs
@@ -2,15 +2,15 @@
 {
     internal static class Constants
     {
-        internal const string EXAMPLE_RESOURCE_HTML_NAMESPACE = "Our.Umbraco.EmbeddedResource.Tests.ExampleResource.html";
+        internal const string EXAMPLE_RESOURCE_HTML_NAMESPACE = "Our.Umbraco.EmbeddedResource.Tests.Resources.ExampleResource.html";
 
         internal const string EXAMPLE_RESOURCE_HTML_URL = "~/AppPlugins/EmbeddedResourceTests/ExampleResource.html";
 
-        internal const string EXAMPLE_RESOURCE_JPG_NAMESPACE = "Our.Umbraco.EmbeddedResource.Tests.ExampleResource.jpg";
+        internal const string EXAMPLE_RESOURCE_JPG_NAMESPACE = "Our.Umbraco.EmbeddedResource.Tests.Resources.ExampleResource.jpg";
 
         internal const string EXAMPLE_RESOURCE_JPG_URL = "~/AppPlugins/EmbeddedResourceTests/ExampleResource.jpg";
 
-        internal const string EXAMPLE_RESOURCE_PNG_NAMESPACE = "Our.Umbraco.EmbeddedResource.Tests.ExampleResource.png";
+        internal const string EXAMPLE_RESOURCE_PNG_NAMESPACE = "Our.Umbraco.EmbeddedResource.Tests.Resources.ExampleResource.png";
 
         internal const string EXAMPLE_RESOURCE_PNG_URL = "~/AppPlugins/EmbeddedResourceTests/ExampleResource.png";
     }
