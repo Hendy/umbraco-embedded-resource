f7eeda52d8200c56af588285803861278c400d87
Hendy Racher <git@hendyracher.co.uk>
2018-06-14 09:19:53 +0100

    updated assembly versions to 0.2.0.0

--- src/Our.Umbraco.EmbeddedResource.Tests/Properties/AssemblyInfo.cs
+++ src/Our.Umbraco.EmbeddedResource.Tests/Properties/AssemblyInfo.cs
@@ -17,8 +17,8 @@ using System.Runtime.InteropServices;
 [assembly: Guid("6ea5a054-b627-4512-b6ed-a3f1bb6141d8")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
 
 // Register each of the test embedded resource files
 [assembly: EmbeddedResource("Our.Umbraco.EmbeddedResource.Tests.EmbeddedResources.EmbeddedResource.html", Constants.HTML_EMBEDDED_RESOURCE_URL)]
