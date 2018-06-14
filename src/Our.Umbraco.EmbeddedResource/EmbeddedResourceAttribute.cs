33ef794ea42e28ba6a1ee383bea96fe726f7daee
Hendy Racher <git@hendyracher.co.uk>
2018-06-14 08:57:07 +0100

    Revert "BREAKING CHANGE: removed attribute constructor overload that had the optional bool param"
    This reverts commit 15c92e753b353c03105efa639eb98f2109328712.

--- src/Our.Umbraco.EmbeddedResource/EmbeddedResourceAttribute.cs
+++ src/Our.Umbraco.EmbeddedResource/EmbeddedResourceAttribute.cs
@@ -26,5 +26,18 @@ namespace Our.Umbraco.EmbeddedResource
             this.ResourceNamespace = resourceNamespace;
             this.ResourceUrl = resourceUrl;
         }
+
+        /// <summary>
+        /// Register an embedded resource in this assembly so it can be served over http(s).
+        /// </summary>
+        /// <param name="resourceNamespace">The full namespace of the embedded resource file to register - eg. "MyProject.Folder.ExampleResource.html"</param>
+        /// <param name="resourceUrl">The app relative url on which the resource file should be served - eg. "~/AppPlugins/MyProject/Folder/ExampleResource.html"</param>
+        /// <param name="backOfficeUserOnly">When true, this resource will only be served to a user logged into the back office</param>
+        [Obsolete("the bool flag will be removed in 0.2.0")]
+        public EmbeddedResourceAttribute(string resourceNamespace, string resourceUrl, bool backOfficeUserOnly = false)
+        {
+            this.ResourceNamespace = resourceNamespace;
+            this.ResourceUrl = resourceUrl;
+        }
     }
 }
