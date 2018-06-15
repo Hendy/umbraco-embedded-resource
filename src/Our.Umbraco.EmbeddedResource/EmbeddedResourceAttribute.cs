124d5497ef7418ad50ddc6b3b1234a0e02366e22
Hendy Racher <git@hendyracher.co.uk>
2018-06-15 13:04:36 +0100

    code formatting

--- src/Our.Umbraco.EmbeddedResource/EmbeddedResourceAttribute.cs
+++ src/Our.Umbraco.EmbeddedResource/EmbeddedResourceAttribute.cs
@@ -3,7 +3,7 @@
 namespace Our.Umbraco.EmbeddedResource
 {
     /// <summary>
-    /// Attribe to register an embedded resource
+    /// Attribute to register an embedded resource
     /// </summary>
     [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
     public class EmbeddedResourceAttribute : Attribute, IEmbeddedResourceAttribute
@@ -33,7 +33,7 @@ namespace Our.Umbraco.EmbeddedResource
         /// <param name="resourceNamespace">The full namespace of the embedded resource file to register - eg. "MyProject.Folder.ExampleResource.html"</param>
         /// <param name="resourceUrl">The app relative url on which the resource file should be served - eg. "~/AppPlugins/MyProject/Folder/ExampleResource.html"</param>
         /// <param name="backOfficeUserOnly">When true, this resource will only be served to a user logged into the back office</param>
-        [Obsolete("This overload wil be removed in 0.3.0")]
+        [Obsolete("This overload wil be removed in 0.3.0 - use EmbeddedResourceProtected instead")]
         public EmbeddedResourceAttribute(string resourceNamespace, string resourceUrl, bool backOfficeUserOnly = false)
         {
             this.ResourceNamespace = resourceNamespace;
