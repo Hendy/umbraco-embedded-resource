8dd76c6cf977cc956b21287a4192b81640a2b31e
Hendy <hendyracher@hotmail.com>
2019-01-06 19:55:47 +0000

    added static public method (that does nothing) so consumer can call to ensure this dll becomes a reference

---
+++ src/Our.Umbraco.EmbeddedResource/Registration.cs
@@ -0,0 +1,14 @@
+﻿namespace Our.Umbraco.EmbeddedResource
+{
+    public static class Registration
+    {
+        /// <summary>
+        ///  Dummy method that can be called by consumer to ensure this dll is associated as a reference
+        ///  (seems attribute useage only doesn't trigger any association)
+        /// </summary>
+        public static void EnsureReference()
+        {
+
+        }
+    }
+}
